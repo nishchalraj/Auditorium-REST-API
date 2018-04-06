210ce8aa095cfaa84950e2ece62bcc0b71b1688c
TERM4 <narendrachouhan307@gmail.com>
2018-04-06 05:09:13 +0000

    new

--- models/user.js
+++ models/user.js
@@ -6,7 +6,7 @@ var userSchema = new Schema({
   name: { type: String, required: true }, 
   user: { type: String, required: true, trim: true , unique: true }, 
   pass: { type: String, required: true },
-  token: { type: String , unique: true , default: "GIVEITATRY" },
+  token: { type: String , default: "GIVEITATRY" },
   isAdmin: { type: Boolean, default: 0 },
    
 },
