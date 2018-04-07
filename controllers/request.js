56cf78b4904804ff36fe8b1b9d81dad538f8f23a
TERM4 <narendrachouhan307@gmail.com>
2018-04-07 21:45:28 +0000

    nn

--- controllers/request.js
+++ controllers/request.js
@@ -66,7 +66,7 @@ exports.createRequest = function(req, res, next) {
                 var date = req.body.date +" "+req.body.etime;
                 date = moment(date); 
                 //date = date.tz('Asia/Calcutta').format("YYYY-MM-DDTHH:MM:ss");
-                date = date.tz('UTC').format("YYYY-MM-DDTHH:MM:ss");
+                date = date.tz('America/New_York').format("YYYY-MM-DDTHH:MM:ss");
                 req.body.expireAt = new Date(date);
 	            if(req.Admin)
 	            req.body.approved = 1;
