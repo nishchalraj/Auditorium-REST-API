b1afe0134741fa654704e1764760d7b7c7500d31
Narendra Singh <narendrasingh@Narendras-MacBook-Pro.local>
2018-04-08 13:04:33 +0530

    GITHUB

--- config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	name: 'AUDI-API',
-	env: process.env.NODE_ENV || 'production',
-	port: process.env.PORT || 80,
-	base_url: 'ADD INTERFACE WITH PORT',
-	db: {
-		uri: 'ADD MONGODB URI',
-	}
-};
